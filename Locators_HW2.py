881e1d87856bebc1cebc5b3a922817c53f36641e
Joshua H <joshunter4@gmail.com>
2024-03-08 21:20:26 -0800

    Homework 2

---
+++ Locators_HW2.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# Amazon Logo
+driver.find_element(By.XPATH, "//i[@aria-label='Amazon']")
+
+# Email field
+driver.find_element(By.ID, 'ap_email')
+
+# Continue button
+driver.find_element(By.ID, 'continue')
+
+# Conditions of use link
+driver.find_element(By.XPATH, "//a[text()='Conditions of Use']")
+
+# Privacy Notice link
+driver.find_element(By.XPATH, "//a[text()='Privacy Notice']")
+
+# Need help link
+driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
+
+# Forgot your password link
+driver.find_element(By.ID, 'auth-fpp-link-bottom')
+
+# Other issues with Sign-In link
+driver.find_element(By.ID, 'ap-other-signin-issues-link')
+
+# Create your Amazon account button
+driver.find_element(By.ID, 'createAccountSubmit')
---
+++ Target_signin.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver_path = ChromeDriverManager().install()
+driver = webdriver.Chrome(service=Service(driver_path))
+driver.maximize_window()
+
+driver.get('https://www.target.com/')
+
+driver.find_element(By.XPATH, "//*[@data-test='@web/AccountLink']").click()
+
+driver.find_element(By.XPATH, "//*[@data-test='accountNav-signIn']").click()
+
+expected = 'Sign into your Target account'
+
+driver.find_element(By.XPATH, "//span[text()='Sign into your Target account']")
+
+driver.find_element(By.ID, 'login')
